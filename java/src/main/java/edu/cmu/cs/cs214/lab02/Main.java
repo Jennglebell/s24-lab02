fafdde24416e84314cc8ae4d1d93c36cbf940db6
jennifer <gwyjennifer@gmail.com>
2024-01-23 22:34:49 -0500

    change main

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,19 @@
 package edu.cmu.cs.cs214.lab02;
 
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.Shape;
 
-public class Main {
+public class Main {    
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
+        Renderer renderer = new Renderer();
 
-        Renderer renderer = new Renderer(rectangle);
+        Shape shape1 = renderer.getShape("CIRCLE");
+        shape1.draw;
 
-        renderer.draw();
+        Shape shape2 = renderer.getShape("RECTANGLE");
+        shape2.draw;
+
+        Shape shape3 = renderer.getShape("SQUARE");
+        shape3.draw;
     }
 }
